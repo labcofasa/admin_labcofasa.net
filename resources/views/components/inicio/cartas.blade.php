f9c85f6f3249a2a8e5f0fb89e531b8b44e97e503
Melvin Alfaro <melvinalfaro@labcofasa.com>
2024-01-19 10:30:42 -0600

    Se corrigieron errores en el estilo

--- resources/views/components/inicio/cartas.blade.php
+++ resources/views/components/inicio/cartas.blade.php
@@ -1,23 +1,21 @@
-<div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 cartas">
+<div class="cartas-inicio">
     @foreach ($aplicaciones as $aplicacion)
-        <div class="col">
-            <div class="btn-app">
-                <a href="{{ $aplicacion->enlace_aplicacion }}" target="_blank">
-                    <div class="card">
-                        <div class="app-image">
-                            @if ($aplicacion->imagen_aplicacion)
-                                <img src="{{ asset('images/aplicaciones/imagen/' . $aplicacion->id . '/' . $aplicacion->imagen_aplicacion) }}"
-                                    alt="Icono aplicación">
-                            @else
-                                <img src="{{ asset('images/logo.svg') }}" alt="Imagen por defecto">
-                            @endif
-                        </div>
-                        <div class="app-name">
-                            <p class="name">{{ $aplicacion->nombre_aplicacion }}</p>
-                        </div>
+        <div class="btn-app">
+            <a href="{{ $aplicacion->enlace_aplicacion }}" target="_blank">
+                <div class="card">
+                    <div class="app-image">
+                        @if ($aplicacion->imagen_aplicacion)
+                            <img src="{{ asset('images/aplicaciones/imagen/' . $aplicacion->id . '/' . $aplicacion->imagen_aplicacion) }}"
+                                alt="Icono aplicación">
+                        @else
+                            <img src="{{ asset('images/logo.svg') }}" alt="Imagen por defecto">
+                        @endif
                     </div>
-                </a>
-            </div>
+                    <div class="app-name">
+                        <p class="name">{{ $aplicacion->nombre_aplicacion }}</p>
+                    </div>
+                </div>
+            </a>
         </div>
     @endforeach
 </div>
