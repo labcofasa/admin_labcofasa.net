b1709c053c65eb0ebfd2ce78ccc0458f535cc570
Poweredget40 <melvinalfaro@gmail.com>
2024-01-19 10:45:50 -0600

    Se revirtieron los cambios en las cartas de inicio

--- resources/views/components/inicio/cartas.blade.php
+++ resources/views/components/inicio/cartas.blade.php
@@ -1,21 +1,24 @@
-<div class="cartas-inicio">
+<div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 cartas">
     @foreach ($aplicaciones as $aplicacion)
-        <div class="btn-app">
-            <a href="{{ $aplicacion->enlace_aplicacion }}" target="_blank">
-                <div class="card">
-                    <div class="app-image">
-                        @if ($aplicacion->imagen_aplicacion)
-                            <img src="{{ asset('images/aplicaciones/imagen/' . $aplicacion->id . '/' . $aplicacion->imagen_aplicacion) }}"
-                                alt="Icono aplicación">
-                        @else
-                            <img src="{{ asset('images/logo.svg') }}" alt="Imagen por defecto">
-                        @endif
+        <div class="col">
+            <div class="btn-app">
+                <a href="{{ $aplicacion->enlace_aplicacion }}" target="_blank">
+                    <div class="card">
+                        <div class="app-image">
+                            @if ($aplicacion->imagen_aplicacion)
+                                <img src="{{ asset('images/aplicaciones/imagen/' . $aplicacion->id . '/' . $aplicacion->imagen_aplicacion) }}"
+                                    alt="Icono aplicación">
+                            @else
+                                <img src="{{ asset('images/logo.svg') }}" alt="Imagen por defecto">
+                            @endif
+                        </div>
+                        <div class="app-name">
+                            <p class="name">{{ $aplicacion->nombre_aplicacion }}</p>
+                        </div>
                     </div>
-                    <div class="app-name">
-                        <p class="name">{{ $aplicacion->nombre_aplicacion }}</p>
-                    </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
     @endforeach
 </div>
+
