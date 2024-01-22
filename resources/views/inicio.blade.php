615e355ffb394c492e348ff630b8e235b82595d2
Melvin Alfaro <melvinalfaro@labcofasa.com>
2024-01-22 09:40:04 -0600

    Se soluciono el problema de la carga de imagen de perfil

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,14 +4,11 @@
 
 @section('contenido')
     <div class="container-fluid content">
-        <div class="card-container">
 
-            <!-- Titulo-->
-            <h1 class="pb-3">@yield('titulo')</h1>
+        <!-- Titulo-->
+        <h1 class="pb-3">@yield('titulo')</h1>
 
-            <!-- Botones para aplicaciones registradas -->
-            <x-inicio.cartas :aplicaciones="$aplicaciones" />
-
-        </div>
+        <!-- Botones para aplicaciones registradas -->
+        <x-inicio.cartas :aplicaciones="$aplicaciones" />
     </div>
 @endsection
