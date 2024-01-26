9d196a6a1ddaa7ffab78e6f6b6dc85dbd317bf13
Melvin Alfaro <melvii22n@gmail.com>
2024-01-25 20:17:15 -0600

    Se mejoro el aspecto de la aplicacion

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,11 +4,13 @@
 
 @section('contenido')
     <div class="container-fluid content">
+        <div class="card-container">
 
-        <!-- Titulo-->
-        <h1 class="pb-3">@yield('titulo')</h1>
+            <!-- Titulo-->
+            <h1 class="pb-3">@yield('titulo')</h1>
 
-        <!-- Botones para aplicaciones registradas -->
-        <x-inicio.cartas :aplicaciones="$aplicaciones" />
+            <!-- Botones para aplicaciones registradas -->
+            <x-inicio.cartas :aplicaciones="$aplicaciones" />
+        </div>
     </div>
 @endsection
