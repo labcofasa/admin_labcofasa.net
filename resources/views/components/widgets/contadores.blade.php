144a5d553246102bd5412d493d7fcf56554aba60
Laboratorios Cofasa <informatica@labcofasa.com>
2024-02-07 16:10:11 -0600

    Se agrego la funcionalidad para que los contadores sean enlaces

--- resources/views/components/widgets/contadores.blade.php
+++ resources/views/components/widgets/contadores.blade.php
@@ -52,74 +52,78 @@
         </div>
     </div>
     <div class="col-xl-3 col-sm-6 mb-xl-0 mb-2">
-        <div class="card card-counter carta-info">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                        <div class="datos-usuarios">
-                            <p class="mb-0 link_name">Roles registrados</p>
-                            <h1 class="font-weight-bolder" id="totalRoles"></h1>
+        <a href="{{ route('pag.roles') }}" class="card-enlace">
+            <div class="card card-counter carta-info">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="datos-usuarios">
+                                <p class="mb-0 link_name">Roles registrados</p>
+                                <h1 class="font-weight-bolder" id="totalRoles"></h1>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-4 text-end">
-                        <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
-                            <svg class="counter-icono icon" xmlns="http://www.w3.org/2000/svg"
-                                enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"
-                                fill="#000000">
-                                <g>
-                                    <path d="M0,0h24v24H0V0z" fill="none" />
-                                </g>
-                                <g>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                                <svg class="counter-icono icon" xmlns="http://www.w3.org/2000/svg"
+                                    enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"
+                                    fill="#000000">
                                     <g>
-                                        <path
-                                            d="M4,18v-0.65c0-0.34,0.16-0.66,0.41-0.81C6.1,15.53,8.03,15,10,15c0.03,0,0.05,0,0.08,0.01c0.1-0.7,0.3-1.37,0.59-1.98 C10.45,13.01,10.23,13,10,13c-2.42,0-4.68,0.67-6.61,1.82C2.51,15.34,2,16.32,2,17.35V20h9.26c-0.42-0.6-0.75-1.28-0.97-2H4z" />
-                                        <path
-                                            d="M10,12c2.21,0,4-1.79,4-4s-1.79-4-4-4C7.79,4,6,5.79,6,8S7.79,12,10,12z M10,6c1.1,0,2,0.9,2,2s-0.9,2-2,2 c-1.1,0-2-0.9-2-2S8.9,6,10,6z" />
-                                        <path
-                                            d="M20.75,16c0-0.22-0.03-0.42-0.06-0.63l1.14-1.01l-1-1.73l-1.45,0.49c-0.32-0.27-0.68-0.48-1.08-0.63L18,11h-2l-0.3,1.49 c-0.4,0.15-0.76,0.36-1.08,0.63l-1.45-0.49l-1,1.73l1.14,1.01c-0.03,0.21-0.06,0.41-0.06,0.63s0.03,0.42,0.06,0.63l-1.14,1.01 l1,1.73l1.45-0.49c0.32,0.27,0.68,0.48,1.08,0.63L16,21h2l0.3-1.49c0.4-0.15,0.76-0.36,1.08-0.63l1.45,0.49l1-1.73l-1.14-1.01 C20.72,16.42,20.75,16.22,20.75,16z M17,18c-1.1,0-2-0.9-2-2s0.9-2,2-2s2,0.9,2,2S18.1,18,17,18z" />
+                                        <path d="M0,0h24v24H0V0z" fill="none" />
                                     </g>
-                                </g>
-                            </svg>
+                                    <g>
+                                        <g>
+                                            <path
+                                                d="M4,18v-0.65c0-0.34,0.16-0.66,0.41-0.81C6.1,15.53,8.03,15,10,15c0.03,0,0.05,0,0.08,0.01c0.1-0.7,0.3-1.37,0.59-1.98 C10.45,13.01,10.23,13,10,13c-2.42,0-4.68,0.67-6.61,1.82C2.51,15.34,2,16.32,2,17.35V20h9.26c-0.42-0.6-0.75-1.28-0.97-2H4z" />
+                                            <path
+                                                d="M10,12c2.21,0,4-1.79,4-4s-1.79-4-4-4C7.79,4,6,5.79,6,8S7.79,12,10,12z M10,6c1.1,0,2,0.9,2,2s-0.9,2-2,2 c-1.1,0-2-0.9-2-2S8.9,6,10,6z" />
+                                            <path
+                                                d="M20.75,16c0-0.22-0.03-0.42-0.06-0.63l1.14-1.01l-1-1.73l-1.45,0.49c-0.32-0.27-0.68-0.48-1.08-0.63L18,11h-2l-0.3,1.49 c-0.4,0.15-0.76,0.36-1.08,0.63l-1.45-0.49l-1,1.73l1.14,1.01c-0.03,0.21-0.06,0.41-0.06,0.63s0.03,0.42,0.06,0.63l-1.14,1.01 l1,1.73l1.45-0.49c0.32,0.27,0.68,0.48,1.08,0.63L16,21h2l0.3-1.49c0.4-0.15,0.76-0.36,1.08-0.63l1.45,0.49l1-1.73l-1.14-1.01 C20.72,16.42,20.75,16.22,20.75,16z M17,18c-1.1,0-2-0.9-2-2s0.9-2,2-2s2,0.9,2,2S18.1,18,17,18z" />
+                                        </g>
+                                    </g>
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </a>
     </div>
     <div class="col-xl-3 col-sm-6 mb-xl-0 mb-2">
-        <div class="card card-counter carta-success">
-            <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                        <div class="datos-usuarios">
-                            <p class="mb-0 link_name">Permisos para roles</p>
-                            <h1 class="font-weight-bolder" id="totalPermisos"></h1>
+        <a href="{{ route('pag.permisos') }}" class="card-enlace">
+            <div class="card card-counter carta-success">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="datos-usuarios">
+                                <p class="mb-0 link_name">Permisos para roles</p>
+                                <h1 class="font-weight-bolder" id="totalPermisos"></h1>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-4 text-end">
-                        <div class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
-                            <svg class="counter-icono icon" xmlns="http://www.w3.org/2000/svg"
-                                enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"
-                                fill="#000000">
-                                <g>
-                                    <rect fill="none" height="24" width="24" />
-                                </g>
-                                <g>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
+                                <svg class="counter-icono icon" xmlns="http://www.w3.org/2000/svg"
+                                    enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"
+                                    fill="#000000">
                                     <g>
-                                        <circle cx="17" cy="15.5" fill-rule="evenodd" r="1.12" />
-                                        <path
-                                            d="M17,17.5c-0.73,0-2.19,0.36-2.24,1.08c0.5,0.71,1.32,1.17,2.24,1.17 s1.74-0.46,2.24-1.17C19.19,17.86,17.73,17.5,17,17.5z"
-                                            fill-rule="evenodd" />
-                                        <path
-                                            d="M18,11.09V6.27L10.5,3L3,6.27v4.91c0,4.54,3.2,8.79,7.5,9.82 c0.55-0.13,1.08-0.32,1.6-0.55C13.18,21.99,14.97,23,17,23c3.31,0,6-2.69,6-6C23,14.03,20.84,11.57,18,11.09z M11,17 c0,0.56,0.08,1.11,0.23,1.62c-0.24,0.11-0.48,0.22-0.73,0.3c-3.17-1-5.5-4.24-5.5-7.74v-3.6l5.5-2.4l5.5,2.4v3.51 C13.16,11.57,11,14.03,11,17z M17,21c-2.21,0-4-1.79-4-4c0-2.21,1.79-4,4-4s4,1.79,4,4C21,19.21,19.21,21,17,21z"
-                                            fill-rule="evenodd" />
+                                        <rect fill="none" height="24" width="24" />
                                     </g>
-                                </g>
-                            </svg>
+                                    <g>
+                                        <g>
+                                            <circle cx="17" cy="15.5" fill-rule="evenodd" r="1.12" />
+                                            <path
+                                                d="M17,17.5c-0.73,0-2.19,0.36-2.24,1.08c0.5,0.71,1.32,1.17,2.24,1.17 s1.74-0.46,2.24-1.17C19.19,17.86,17.73,17.5,17,17.5z"
+                                                fill-rule="evenodd" />
+                                            <path
+                                                d="M18,11.09V6.27L10.5,3L3,6.27v4.91c0,4.54,3.2,8.79,7.5,9.82 c0.55-0.13,1.08-0.32,1.6-0.55C13.18,21.99,14.97,23,17,23c3.31,0,6-2.69,6-6C23,14.03,20.84,11.57,18,11.09z M11,17 c0,0.56,0.08,1.11,0.23,1.62c-0.24,0.11-0.48,0.22-0.73,0.3c-3.17-1-5.5-4.24-5.5-7.74v-3.6l5.5-2.4l5.5,2.4v3.51 C13.16,11.57,11,14.03,11,17z M17,21c-2.21,0-4-1.79-4-4c0-2.21,1.79-4,4-4s4,1.79,4,4C21,19.21,19.21,21,17,21z"
+                                                fill-rule="evenodd" />
+                                        </g>
+                                    </g>
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </a>
     </div>
 </div>
