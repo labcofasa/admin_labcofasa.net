352168a550b9dbb76a538e9a1948218c8bc31213
Laboratorios Cofasa <informatica@labcofasa.com>
2024-02-27 16:55:44 -0600

    Se agrego el primer diseño para grafico

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,17 +11,19 @@
             <div class="col-sm-6">
                 <div class="card-chart">
                     <header class="card-chart-header">
-                        <div>
-                            <h2>Ventas</h2>
-                            <span>Seleccionar por año</h3>
-                        </div>
-                        <div>
-                            <button type="button" onclick="selectYear(this, 2023)">
+                        <div class="row justify-content-between">
+                            <div class="col-6">
+                                <h2>Ventas</h2>
+                                <span>Seleccionar por año</h3>
+                            </div>
+                            <div class="col-6 text-end">
+                                <button type="button" onclick="selectYear(this, 2023)">
                                 <span>2023</span>
-                            </button>
-                            <button type="button" class="active" onclick="selectYear(this, 2024)">
-                                <span>2024</span>
-                            </button>
+                                </button>
+                                <button type="button" class="active" onclick="selectYear(this, 2024)">
+                                    <span>2024</span>
+                                </button>
+                            </div>
                         </div>
                     </header>
                     <canvas id="areaTableChart" width="400" height="220"></canvas>
