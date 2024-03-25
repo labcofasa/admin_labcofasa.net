16a334e2bf9fdc59503fff68ef9b40b26bd3a520
Laboratorios Cofasa <informatica@labcofasa.com>
2024-03-25 15:13:53 -0600

    Correccion de errores

--- resources/views/components/formularios/modal.blade.php
+++ resources/views/components/formularios/modal.blade.php
@@ -6,11 +6,20 @@
                 <h1 class="modal-title fs-6" id="formccModalLabel"><span class="fw-bold">Importante:</span> Descargue y
                     complete la carta de
                     responsabilidad</h1>
+                <button class="btn-icon-close" data-bs-dismiss="modal">
+                    <svg class="icon-close" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24"
+                        width="24px" fill="#000000">
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path
+                            d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z" />
+                    </svg>
+                </button>
             </div>
             <div class="modal-body">
                 <div class="small">
                     Antes de comenzar el formulario en línea, por favor descargue el archivo "Carta de Responsabilidad"
-                    utilizando el botón <span class="fw-bold">"Descargar"</span>.<br><br> Este documento es esencial
+                    utilizando el botón <span class="fw-bold">"Descargar carta de responbilidad"</span>.<br><br> Este
+                    documento es esencial
                     para
                     completar el proceso, recuerde que al finalizar el formulario, deberá adjuntar una foto o un archivo
                     de la carta de responsabilidad firmada o sellada.
@@ -18,8 +27,7 @@
 
             </div>
             <div class="modal-footer">
-                <button data-bs-dismiss="modal" aria-label="Close" class="btn btn-secondary">Cerrar</button>
-                <a href="{{ route('descargar.carta') }}" class="btn btn-primary">Descargar</a>
+                <a href="{{ route('descargar.carta') }}" class="btn btn-primary">Descargar carta de responbilidad</a>
             </div>
         </div>
     </div>
