e584ffee73db1afd5e29bfe8cb17b3af041cdc14
Laboratorios Cofasa <informatica@labcofasa.com>
2024-04-15 15:50:20 -0600

    Se agregaron mejoras al sistema de autenticacion

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,12 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="" class="logo" alt="Logo Cofasa">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+                <img src="https://app.labcofasa.net/images/empresas/logo/1/cofasalogo.png" class="logo">
+            @else
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>
