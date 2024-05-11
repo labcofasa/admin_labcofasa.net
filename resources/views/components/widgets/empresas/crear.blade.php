a4bd9bf7e8827cc386c95fbc01b08b128d81046d
unknown <melvii22n@gmail.com>
2024-05-11 13:42:03 -0600

    Se mejoro la interfaz

--- resources/views/components/widgets/empresas/crear.blade.php
+++ resources/views/components/widgets/empresas/crear.blade.php
@@ -9,10 +9,10 @@
                         'admin_giros_ver',
                         'admin_paises_ver',
                     ]))
-                <button class="btn btn-lg btn-primary d-none d-xl-block dropdown-toggle" type="button"
+                <button class="btn btn-lg btn-success d-none d-xl-block dropdown-toggle" type="button"
                     data-bs-toggle="dropdown" aria-expanded="false" ata-bs-auto-close="outside">
                     <div class="accion">
-                        <svg class="icon-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                        <svg class="icon-success" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                             height="24" color="#000000" fill="none">
                             <path
                                 d="M2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2"
